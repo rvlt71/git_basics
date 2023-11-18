8d27751db3c9bcaa91101ecf1b8a02620c8e1f7c
Joshua Tsang <joshua.tsang@soundmouse.com>
2023-11-18 19:16:29 +0000

    fed lines of the text into freq analysis

--- frequency_analysis.py
+++ frequency_analysis.py
@@ -1,20 +1,24 @@
+
 f = open('testfile.txt')
 
 lines = f.readlines()
 print(lines)
 
-for y in lines :
-    print(y)
-    print(len(lines))
+print("Number of lines detected: ", len(lines))
+
+#for line in lines :
+#    print(line)
+
 
 char_freq = {}
-print(char_freq)
+#print(char_freq)
 
-for x in f :
-    print(x)
-    if x in char_freq:
-        char_freq[x] += 1
-    else:
-        char_freq[x] = 1
+for line in lines:
+    print("The current line is: ", line)
+    for letter in line:
+        if letter in char_freq:
+            char_freq[letter] += 1
+        else:
+            char_freq[letter] = 1
 
 print(char_freq)
